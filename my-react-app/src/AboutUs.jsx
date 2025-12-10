adda122d96bdb3bbb49db048007d90a0fc104a16
Andres <hospitablepro1@gmail.com>
2025-12-10 16:48:59 -0600

    Basically finnished about us
    kjkb

--- my-react-app/src/AboutUs.jsx
+++ my-react-app/src/AboutUs.jsx
@@ -1,12 +1,34 @@
 import { Outlet } from "react-router-dom";
 import "./AboutUs.css";
+import Me from './assets/Me.jpeg';
 
 function AboutUs(){
     return (
         <>
         <div className="container2">
             <div className="title2">
-                <h1 className="AboutUsFont">About Us</h1>
+                <h1 className="AboutUsFont">About Me</h1>
+            </div>
+            <div className="bothContainers">
+                <div className="leftSideContainer">
+                    <img className="img2" src={Me}/>
+                </div>
+                <div className="rightSideContainer">
+                    <div className="capsule2">
+                        <p className="indented">
+                            Hello, my name is Andres Quesada and I am a student at UIC. With great pleasure, I present to 
+                            you the first website that I have created!!!
+                            
+                            The purpose of this website is for me to learn more about how to use API's and how I could implement 
+                            better CSS. This will get me closer to my goal of using fullstack frameworks
+                        </p>
+                        <p></p>
+                        <p className="indented">
+                            Aside from coding, I also like to run, play games, and watch anime. Maybe one day, I could make a 
+                            website dedicated to those topics!!!
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
         </>
