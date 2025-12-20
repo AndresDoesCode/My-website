81918619bba4a77d65e945dc0e3321c3688cc506
Andres <hospitablepro1@gmail.com>
2025-12-20 16:34:42 -0600

    Filter Results
    I finnished implementing filter results

--- my-react-app/src/CryptoTracking.jsx
+++ my-react-app/src/CryptoTracking.jsx
@@ -1,18 +1,19 @@
 import { useEffect, useState } from "react";
 import "./CryptoTracking.css";
 
-//Using /ping endpoint
-//https://api.coingecko.com/api/v3/ping?x_cg_demo_api_key=CG-BQaP3tWqquD6rdc2uAVgbEPM
-
-
 function CryptoTracking(){
   const [coins, setCoins] = useState([]);
+  const [name, setName] = useState("");
 
   useEffect(()=>{
-    fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin&names=Bitcoin&symbols=btc&category=layer-1&price_change_percentage=1h&precision=2&order=market_cap_desc')
+    fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=1h&precision=2')
       .then(res => res.json())
       .then(data => setCoins(data))
   }, []);
+
+  function handleNameChange({target}){
+    setName(target.value);
+  }
 
   return (
     <>
@@ -22,14 +23,22 @@ function CryptoTracking(){
       </div>
       <div className="crypto-bottom">
         <div className="capsule3">
-          <ul className="coin-list">
-            {coins.map((coin)=>(
-              <li key={coin.id}>
-                <img src={coin.image} alt={coin.name} width="20" />
-                {coin.name} – ${coin.current_price}
-              </li>
-            ))}
-          </ul>
+          <div className="search">
+            <div className="searchTitle">
+              <h1>Filter Results</h1>
+            </div>
+            <input className="searchBar" value={name} onChange={handleNameChange} placeholder="Filter Results"/>
+          </div>
+          <div className="boxList">
+            {coins.map((coin)=>
+              coin.name.toLowerCase().includes(name.toLowerCase()) ? (
+                <div className="box">
+                  <img src={coin.image} alt={coin.name} width="20" />
+                    {coin.name} – ${coin.current_price}
+                </div>
+              ) : null
+            )}
+          </div>
         </div>
       </div>
     </div>
